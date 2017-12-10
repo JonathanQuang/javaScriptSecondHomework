049474062bc3d06b1e56d6ae6db7a949858854ab
Iris Tao <mtao@stuy.edu>
2017-12-10 16:23:31 -0500

    tmp

--- intro.js
+++ intro.js
@@ -1,5 +1,7 @@
 var theList = document.getElementById('thelist')
-console.log(theList);
+var heading = document.getElementById('h')
+
+//console.log(theList);
 
 var addToList = function(){
 	var node = document.createElement("LI");
@@ -7,3 +9,39 @@ var addToList = function(){
 	node.appendChild(textnode);
 	theList.appendChild(node);	
 };
+
+b.addEventListener('click', addToList);
+
+
+// -------------------------------------------------//
+//this is not finished yet...:///
+
+var changeHeading = function(e){
+	var textnode = document.createTextNode(e);
+	var item = document.getElementById('h');
+	h.replaceChild(textnode, item.childNodes[0]);
+}
+
+var callback = function(){
+	changeHeading(e);
+}
+
+
+var changeBack = function(){
+	var worldtext = document.createTextNode("Hello Worldddd!");
+	heading.replaceChild(worldtext, heading.childNodes[0]);
+}
+
+
+var e = theList.childNodes[1].childNodes[0].nodeValue;
+theList.childNodes[1].addEventListener('mouseover', callback);
+theList.childNodes[1].addEventListener('mouseleave', changeBack);
+
+
+e = theList.childNodes[3].childNodes[0].nodeValue;
+theList.childNodes[9].addEventListener('mouseover', callback);
+//theList.childNodes[3].addEventListener('mouseleave', changeBack);
+
+
+
+
