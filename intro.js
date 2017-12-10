a00129f04d803358c30d2d21832ac0adc93bfe58
Iris Tao <mtao@stuy.edu>
2017-12-10 16:47:51 -0500

    tmp2

--- intro.js
+++ intro.js
@@ -1,7 +1,5 @@
 var theList = document.getElementById('thelist')
-var heading = document.getElementById('h')
 
-//console.log(theList);
 
 var addToList = function(){
 	var node = document.createElement("LI");
@@ -12,35 +10,53 @@ var addToList = function(){
 
 b.addEventListener('click', addToList);
 
-
 // -------------------------------------------------//
-//this is not finished yet...:///
 
-var changeHeading = function(e){
-	var textnode = document.createTextNode(e);
-	var item = document.getElementById('h');
-	h.replaceChild(textnode, item.childNodes[0]);
-}
-
-var callback = function(){
-	changeHeading(e);
-}
-
+var heading = document.getElementById('h');
 
 var changeBack = function(){
+
 	var worldtext = document.createTextNode("Hello Worldddd!");
 	heading.replaceChild(worldtext, heading.childNodes[0]);
 }
 
 
-var e = theList.childNodes[1].childNodes[0].nodeValue;
-theList.childNodes[1].addEventListener('mouseover', callback);
-theList.childNodes[1].addEventListener('mouseleave', changeBack);
+var e = "hyyy";
+
+var DisplayItemName = function(){
+	var itemL = document.getElementsByTagName('li');
+	var len = itemL.length;
+	for (i = 0; i < len; i++){
+		itemL[i].addEventListener('mouseover', callback);
+		e = i;
+		//document.getElementsByTagName('li')[i].childNodes[0].getValue;
+		itemL[i].addEventListener('mouseout', changeBack);
+	}
+}
 
 
-e = theList.childNodes[3].childNodes[0].nodeValue;
-theList.childNodes[9].addEventListener('mouseover', callback);
-//theList.childNodes[3].addEventListener('mouseleave', changeBack);
+var changeHeading = function(e){
+	var orighead = heading.childNodes[0];
+	var newhead = document.createTextNode(e);
+	h.replaceChild(newhead, orighead);
+}
+
+
+var callback = function(){
+	changeHeading(e);
+}
+
+DisplayItemName();
+
+
+// var e = theList.childNodes[1].childNodes[0].nodeValue;
+// theList.childNodes[1].addEventListener('mouseover', callback);
+// theList.childNodes[1].addEventListener('mouseleave', changeBack);
+
+
+// e = theList.childNodes[3].childNodes[0].nodeValue;
+// theList.childNodes[3].addEventListener('mouseover', callback);
+// theList.childNodes[3].addEventListener('mouseleave', changeBack);
 
 
 
