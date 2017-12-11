ee81b464d0584956fc98a2bd3671fa5c9bbbaf91
Iris Tao <mtao@stuy.edu>
2017-12-10 20:52:00 -0500

    updated

--- intro.js
+++ intro.js
@@ -1,6 +1,8 @@
+//thelist
 var theList = document.getElementById('thelist')
 
 
+//add an element to the list
 var addToList = function(){
 	var node = document.createElement("LI");
 	var textnode = document.createTextNode('hi');
@@ -8,59 +10,47 @@ var addToList = function(){
 	theList.appendChild(node);	
 };
 
+//when the btn is clicked, addToList is called 
 b.addEventListener('click', addToList);
 
-// -------------------------------------------------//
 
+//heading
 var heading = document.getElementById('h');
 
-var changeBack = function(){
 
-	var worldtext = document.createTextNode("Hello Worldddd!");
+//swap the original text in the header with the new text
+var changeHeading = function(){
+	var orighead = heading.childNodes[0];
+	var newhead1 = document.createTextNode(this.childNodes[0].nodeValue);
+	h.innerHTML = this.childNodes[0].nodeValue;
+}
+
+
+//change the header back to "Hello World!""
+var changeBack = function(){
+	var worldtext = document.createTextNode("Hello World!");
 	heading.replaceChild(worldtext, heading.childNodes[0]);
 }
 
-
-var e = "hyyy";
-
-var DisplayItemName = function(){
-	var itemL = document.getElementsByTagName('li');
-	var len = itemL.length;
-	for (i = 0; i < len; i++){
-		
-		e = document.getElementsByTagName('li')[i].childNodes[0].nodeValue;
-		console.log(e);
-
-		itemL[i].addEventListener('mouseover', callback);
-
-
-		itemL[i].addEventListener('mouseout', changeBack);
-	}
+//remove an element from the DOM
+var removeElement = function(){
+	this.remove();
 }
 
-//e = "hy";
-var changeHeading = function(e){
-	var orighead = heading.childNodes[0];
-	var newhead = document.createTextNode(e);
-	h.replaceChild(newhead, orighead);
+//a list containing all the list items in thelist 
+var items = document.getElementsByTagName('li');
+
+//loop through all items and initiate specific callback functions according to mouse actions
+for (i=0; i<items.length; i++){
+	items[i].addEventListener('mouseover', changeHeading);
+	items[i].addEventListener('mouseout', changeBack);
+	items[i].addEventListener('click', removeElement);
 }
 
 
-var callback = function(){
-	changeHeading(e);
-}
-
-DisplayItemName();
 
 
-// var e = theList.childNodes[1].childNodes[0].nodeValue;
-// theList.childNodes[1].addEventListener('mouseover', callback);
-// theList.childNodes[1].addEventListener('mouseleave', changeBack);
 
-
-// e = theList.childNodes[3].childNodes[0].nodeValue;
-// theList.childNodes[3].addEventListener('mouseover', callback);
-// theList.childNodes[3].addEventListener('mouseleave', changeBack);
 
 
 
